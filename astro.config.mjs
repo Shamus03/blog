e16280c5e753b3d8d9618fb105375a4f2b55aff8
Shamus Taylor <Shamus03@me.com>
2023-05-29 23:37:11 -0500

    Add trailingslash never

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,7 @@ import { SITE } from "./src/config";
 // https://astro.build/config
 export default defineConfig({
   site: SITE.website,
+  trailingSlash: 'never',
   integrations: [
     tailwind({
       config: {
